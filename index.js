a221e2b3ac5865e6ea5659e12d63a4bd45cdc99c
dung022002 <dungvd02@gmail.com>
2023-03-29 15:41:16 +0700

    commit2

--- index.js
+++ index.js
@@ -11,24 +11,33 @@ async function run() {
     let $ = cheerio.load(res.data)
     const arrOne = $('div.inner_list_dethi').children()
     const arr = []
+    const arrImage = []
     arrOne.each((index, element) => {
         const link = $(element).find('div.dethi_item>div.top_dethi>span.btn_status > a').attr('href')
-        // arr.push() day vao sau cung
+        // arr.push() đẩy vào sau cùng của dãy
         arr[index] = 'https://vnexpress.net' + link
 
     })
-    //kiem tra thu muc da ton tai hay chua
-    if (!!fs.existsSync('./Bo_de_thi_ly_thuyet')) {
+    // kiểm tra thư mục tồn tại hay chưa
+    if (fs.existsSync('./Bo_de_thi_ly_thuyet')) {
 
-        //xoa thu muc da ton tai
+        // xóa thư mục nếu đã tồn tại
         fs.rmSync('./Bo_de_thi_ly_thuyet', { recursive: true })
     }
 
-    //tao 1 thu muc moi
+    // tạo 1 thư mục mới
     fs.mkdirSync('./Bo_de_thi_ly_thuyet')
 
 
+    if (fs.existsSync('public/images')) {
+        fs.rmSync('public/images', { recursive: true })
+    }
+    fs.mkdirSync('public/images')
+
+    // lặp vào từng link trong mảng
     for (const [index, iterator] of arr.entries()) {
+
+        //tạo 1 json tương đương với 1 đề
         const test = {
             name: 'Đề ' + (index + 1),
             listQuestion: []
@@ -46,19 +55,44 @@ async function run() {
 
             // get question content
             const questionContent = a(question).find('p.noidung_cauhoi').text()
+            const qsContent = questionContent.replaceAll('\n', '')
 
             // get children of list ans
             const listAnswers = a(question).find('div.noidung_dapan > ul').children()
 
-            //define question obj
-            const questionJson = { name: nameQs, content: questionContent, image: null, answers: [] }
 
-            // find img if exist add image to question obj
+            let pathImg = null
+
+            // find img if exist add image to question obj 
             const imgLink = a(question).find('p.noidung_cauhoi> img').attr('src')
             // if (imgLink) {
             //     questionJson.image = imgLink
             // }
-            questionJson.image = imgLink ? imgLink : null // Toan tu 3 ngoi
+
+            if (imgLink) {
+                arrImage.push(imgLink);
+                const nameImg = imgLink.split('https://i-vnexpress.vnecdn.net/2020/09/04/')[1];
+                pathImg = '/images/' + nameImg;
+
+                axios({
+                    url: imgLink,
+                    responseType: 'stream',
+                }).then(res => {
+                    res.data.pipe(fs.createWriteStream('public/images/' + nameImg)).on('finish', () => {
+
+                    });
+                })
+                    .catch(err => {
+                        console.log(nameImg);
+                        return reject(err);
+                    })
+
+            }
+
+
+            //define question obj
+            const questionJson = { name: nameQs, content: qsContent, image: pathImg, answers: [] }
+
 
             listAnswers.each((i, el) => {
                 // push ans to list ans
--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.engine('hbs', hbs.engine({
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'css')))
+app.use(express.static(path.join(__dirname, 'public')))
 // define sub expresion hbs
 Handlebars.registerHelper('isNotNull', function (value) {
     return value !== null;
